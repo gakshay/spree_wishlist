67cde359a5778c88d17d75f44fae602e1099877c
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-07-28 12:32:32 +0530

    Rollback to 06679c64717547b38dbe7aba6226da606410c0e1, Error: uninitialized constant Deface

--- lib/spree_wishlist_hooks.rb
+++ lib/spree_wishlist_hooks.rb
@@ -1,17 +1,9 @@
-Deface::Override.new(:virtual_path => "products/show",
-                     :name => "add_wish_to_cart_form",
-                     :insert_bottom => "[data-hook='cart_form']",
-                     :partial => "products/wishlist_form",
-                     :disabled => false)
-Deface::Override.new(:virtual_path => "users/show",
-                     :name => "add_wishlists_to_account_my_orders",
-                     :insert_after => "[data-hook='account_my_orders']",
-                     :partial => "users/wishlists",
-                     :disabled => false)
-Deface::Override.new(:virtual_path => "wishlists/show",
-                     :name => "add_email_to_wishlist_footer_links",
-                     :insert_after => "[data-hook='wishlist_footer_links']",
-                     :text => %(<div id="email_to_friend">
-                       <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
-                     </div>),
-                     :disabled => false)
+class SpreeWishlistHooks < Spree::ThemeSupport::HookListener
+  insert_after :cart_form, 'products/wishlist_form'
+  insert_after :account_my_orders, 'users/wishlists'
+  insert_after :wishlist_footer_links do
+    %(<div id="email_to_friend">
+      <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
+    </div>)
+  end
+end
